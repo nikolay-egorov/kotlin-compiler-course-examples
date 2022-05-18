9c9ab39bce5b8db0e1bd85988c2c8720335c8373
nikolay-egorov <deathadder98@gmail.com>
2022-05-18 09:17:49 +0300

    Diagnostics tests

---
+++ testData/diagnostics/logging.kt
@@ -0,0 +1,32 @@
+import org.itmo.logging.plugin.annotations.StateLogging
+import org.itmo.logging.plugin.annotations.ToLogFunction
+
+
+@StateLogging
+open class A {
+
+}
+
+@StateLogging
+class <!DUPLICATING_ANNOTATIONS!>B<!>: A() {
+    private val x = 47
+
+    @ToLogFunction(false)
+    fun foo(y: Int) = x + y
+}
+
+class C {
+    var x = 1
+    @ToLogFunction(true)
+    fun <!NO_LOGGER_CLASS_ANNOTATION, RETURN_HAS_NO_EFFECT!>withEffect<!>() {
+        x = 42
+    }
+}
+@StateLogging
+enum class <!TARGET_SHOULD_NOT_BE_ENUM_CLASS!>WrongUsage<!>
+
+<!WRONG_ANNOTATION_TARGET!>@ToLogFunction<!>
+val z = 1
+
+<!WRONG_ANNOTATION_TARGET!>@StateLogging<!>
+val asClass = 42
